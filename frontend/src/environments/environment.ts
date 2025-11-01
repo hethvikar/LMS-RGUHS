6b77f78fbccba7cae2f6cbe0d722ae56918df9dd
raju <hethvikar55@gmail.com>
2025-11-01 21:41:22 +0530

    fix: resume extact

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
   apiUrl: "https://localhost:5001/api",
   appName: "Placement Cell System",
   version: "1.0.0",
-  builderApiKey: "2e4765280900489e9476914ad9c61a29",
-  openAiApiKey: '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'
+  builderApiKey: "2e4765280900489e9476914ad9c61a29"
 };
